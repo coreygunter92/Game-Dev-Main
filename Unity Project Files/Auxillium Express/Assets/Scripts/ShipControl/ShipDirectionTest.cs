8b06f628b0e116d4630857ff3a43ac64602c6178
coreygunter92 <corey.s.gunter@gmail.com>
2018-01-16 16:34:40 -0700

    WaypointLandingPadUpdate
    Added Landing Pads. Added Waypoints.

---
+++ Unity Project Files/Auxillium Express/Assets/Scripts/ShipControl/ShipDirectionTest.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ShipDirectionTest : MonoBehaviour {
+
+    public float shipSpeed = 5f;
+    public float hullHealth = 100;
+
+    public List<Transform> idleWaypoints;
+    public List<Transform> dockingWaypoints;
+    public List<Transform> undockingWaypoints;
+    public List<Transform> exitWaypoints;
+
+
+
+    void Start()
+    {
+
+    }
+
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(Vector3.left * Time.deltaTime);	
+	}
+}
