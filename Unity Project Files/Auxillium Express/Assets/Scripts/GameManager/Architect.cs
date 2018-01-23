59231e577beb7aae6a9df49cab65234cd5ba7e56
coreygunter92 <corey.s.gunter@gmail.com>
2018-01-22 19:55:07 -0700

    ShipMovement
    Added ship spawning and movement. Prototype state machine created.

--- Unity Project Files/Auxillium Express/Assets/Scripts/GameManager/Architect.cs
+++ Unity Project Files/Auxillium Express/Assets/Scripts/GameManager/Architect.cs
@@ -5,15 +5,24 @@ using System.Linq;
 
 public class Architect : MonoBehaviour {
 
+    public GameObject[] entranceWaypoints;
     public GameObject[] idleWaypoints;
     public GameObject[] dockingWaypoints1;
     public GameObject[] dockingWaypoints2;
     public GameObject[] undockingWaypoints1;
     public GameObject[] undockingWaypoints2;
     public GameObject[] exitWaypoints;
+    public Transform shipSpawn;
+
+
+    private int maxActiveShips = 3;
+    public int activeShips;
+    public GameObject shipPrefab;
+    public float timeSinceLastSpawned;
 
     void SetupWaypoints()
     {
+        entranceWaypoints = GameObject.FindGameObjectsWithTag("entranceWaypoints").OrderBy(go => go.name).ToArray();
         idleWaypoints = GameObject.FindGameObjectsWithTag("idleWaypoint").OrderBy(go => go.name).ToArray();
         dockingWaypoints1 = GameObject.FindGameObjectsWithTag("dockingWaypoint1").OrderBy(go => go.name).ToArray();                           //self explanatory Sets up waypoints
         dockingWaypoints2 = GameObject.FindGameObjectsWithTag("dockingWaypoint2").OrderBy(go => go.name).ToArray();
@@ -22,6 +31,7 @@ public class Architect : MonoBehaviour {
         undockingWaypoints2 = GameObject.FindGameObjectsWithTag("dockingWaypoint2").OrderBy(go => go.name).ToArray();
         System.Array.Reverse(undockingWaypoints2);
         exitWaypoints = GameObject.FindGameObjectsWithTag("exitWaypoint").OrderBy(go => go.name).ToArray();
+        shipSpawn = GameObject.FindGameObjectWithTag("shipSpawn").transform;
     }
 
     void Awake()
@@ -29,15 +39,33 @@ public class Architect : MonoBehaviour {
         SetupWaypoints();
     }
 
-    // Use this for initialization
     void Start()
     {
-
+        activeShips = GameObject.FindGameObjectsWithTag("NPCShip").Length;
+        timeSinceLastSpawned = Time.time;
     }
 
-    // Update is called once per frame
+
     void Update()
     {
+        timeSinceLastSpawned += Time.deltaTime;
+        //if there are not maximum number of ships on the field
+        if(activeShips <= maxActiveShips && timeSinceLastSpawned >= 5f)
+        {
+            
+            GameObject damagedShip = Instantiate(shipPrefab, shipSpawn.position, shipSpawn.rotation);
+            timeSinceLastSpawned = 0;
+            activeShips++;
+        }
+        else
+        {
 
+        }
+        //and i haven't spawned a ship recently
+        //spawn a ship with random stats
+        //at the spawn marker
+
+        Debug.Log(shipSpawn.transform);
+        //Debug.Log(timeSinceLastSpawned);
     }
 }
--- Unity Project Files/Auxillium Express/Assets/Scripts/ShipControl/ShipDirectionTest.cs
+++ Unity Project Files/Auxillium Express/Assets/Scripts/ShipControl/ShipDirectionTest.cs
@@ -5,20 +5,92 @@ using UnityEngine;
 
 public class ShipDirectionTest : MonoBehaviour {
     public GameObject gameManager;
+    public enum SHIPSTATES { ENTRANCE, IDLE, DOCKING, UNDOCKING, EXIT };
 
-    public float shipSpeed = 5f;
-    public float hullHealth = 100;
+    public SHIPSTATES executeState(SHIPSTATES state)
+    {
+        if (state == SHIPSTATES.ENTRANCE)
+        {
+           
+            if (currentWaypoint != entranceWaypoints.Length)
+            {
+                Vector3 startPosition = entranceWaypoints[currentWaypoint].transform.position;
+                Vector3 endPosition = entranceWaypoints[currentWaypoint + 1].transform.position;
 
+                float pathLength = Vector3.Distance(startPosition, endPosition);
+                float totalTimeForPath = pathLength / shipSpeed;
+                float currentTimeOnPath = Time.time - lastWaypointSwitchTime;
+
+                gameObject.transform.position = Vector3.Lerp(startPosition, endPosition, currentTimeOnPath / totalTimeForPath);
+
+                if (gameObject.transform.position.Equals(endPosition))
+                {
+
+                    if (currentWaypoint < entranceWaypoints.Length - 2)
+                    {
+
+                        currentWaypoint++;
+                        lastWaypointSwitchTime = Time.time;
+
+
+                    }
+                    
+                }
+
+                transform.LookAt(endPosition);
+            }
+            else
+            {
+                previousState = SHIPSTATES.ENTRANCE;
+                currentState = SHIPSTATES.IDLE;
+            }
+            
+        }
+        else if (state == SHIPSTATES.IDLE)
+        {
+
+        }
+        else if (state == SHIPSTATES.DOCKING)
+        {
+
+        }
+        else if (state == SHIPSTATES.UNDOCKING)
+        {
+
+        }
+        else if (state == SHIPSTATES.EXIT)
+        {
+
+        }
+
+        return state;
+    }
+
+    public GameObject[] entranceWaypoints;
     public GameObject[] idleWaypoints;
     public GameObject[] dockingWaypoints1;
     public GameObject[] dockingWaypoints2;
     public GameObject[] undockingWaypoints1;
     public GameObject[] undockingWaypoints2;
     public GameObject[] exitWaypoints;
+    public int previousWaypoint;
+    public int currentWaypoint;
+    public int nextWaypoint;
+    public SHIPSTATES previousState;
+    public SHIPSTATES currentState;
+
+    public Vector3 Velocity;
+    public Vector3 MoveDirection;
+    public Vector3 Target;
+    public Vector3 endPointDirection;
+    private float lastWaypointSwitchTime;
+    public float shipSpeed = 5f;
+    public float hullHealth = 100;
 
     void TranslateWaypoints()
     {
         gameManager = GameObject.FindGameObjectWithTag("Game Manager");
+        entranceWaypoints = gameManager.GetComponent<Architect>().entranceWaypoints;
         idleWaypoints = gameManager.GetComponent<Architect>().idleWaypoints;
         dockingWaypoints1 = gameManager.GetComponent<Architect>().dockingWaypoints1;
         dockingWaypoints2 = gameManager.GetComponent<Architect>().dockingWaypoints2;
@@ -30,15 +102,25 @@ public class ShipDirectionTest : MonoBehaviour {
     void Awake()
     {
         TranslateWaypoints();
+
     }
 
     void Start()
     {
+        
+        currentState = SHIPSTATES.ENTRANCE;
+        currentWaypoint = 0;
+        lastWaypointSwitchTime = Time.time;
+       
 
+        //roll random values for ship
     }
 
 	// Update is called once per frame
 	void Update () {
-        transform.Translate(Vector3.left * Time.deltaTime);	
+        executeState(currentState);
+
+        Debug.Log(currentState);
+        Debug.Log(currentWaypoint);
 	}
 }
